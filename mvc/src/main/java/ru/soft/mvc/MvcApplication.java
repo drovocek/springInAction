c377a4ce170ad60b69afdead059eeeabe4605211
drovocek <mycodepg@gmail.com>
2022-11-04 13:01:14 +0300

    add tests

--- mvc/src/main/java/ru/soft/mvc/MvcApplication.java
+++ mvc/src/main/java/ru/soft/mvc/MvcApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MvcApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MvcApplication.class);
+        SpringApplication.run(MvcApplication.class, args);
     }
 }
---
+++ mvc/src/test/java/ru/soft/mvc/MvcApplicationTest.java
@@ -0,0 +1,12 @@
+package ru.soft.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcApplicationTest {
+
+    @Test
+    void main() {
+    }
+}
